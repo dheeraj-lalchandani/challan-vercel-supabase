be4d8733bff3c59cc38718fd383af020e358f2f7
Dheeraj Lalchandani <dheerajlalchandani@Dheerajs-MacBook-Air.local>
2025-05-17 17:49:02 +0530

    Test with approved IP 49.206.1.121

--- pages/api/process-csv.js
+++ pages/api/process-csv.js
@@ -81,6 +81,7 @@ export default async function handler(req, res) {
           'X-Ipay-Auth-Code': '1',
           'X-Ipay-Client-Id': process.env.IPAY_CLIENT_ID,
           'X-Ipay-Client-Secret': process.env.IPAY_CLIENT_SECRET,
+          'X-Ipay-Endpoint-Ip': '49.206.1.121',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
